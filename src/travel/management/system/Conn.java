a41a5135d803ee1a09f39ab6d2082c254eaf8f92
chbhargavi2 <113128726+chbhargavi2@users.noreply.github.com>
2025-04-29 20:43:48 -0400

    Conn.java

--- src/travel/management/system/Conn.java
+++ src/travel/management/system/Conn.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package travel.management.system;
 
 import java.sql.*;  
@@ -12,8 +7,8 @@ public class Conn{
     Statement s;
     public Conn(){  
         try{  
-            Class.forName("com.mysql.jdbc.Driver");  
-            c =DriverManager.getConnection("jdbc:mysql:///tms","root","root"); 
+            Class.forName("com.mysql.cj.jdbc.Driver");  
+            c =DriverManager.getConnection("jdbc:mysql:///tms","root","root1234"); 
             
             s =c.createStatement();  
             
